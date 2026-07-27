4f413a31830196685779ea0b95205645955517b0
code-verse-dev <155548702+code-verse-dev@users.noreply.github.com>
2026-07-27 19:35:44 +0500

    Add deploy files from template

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "sports-card-backend",
+      script: "dist/index.js",
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      env: {
+        NODE_ENV: "customdev",
+      },
+    },
+  ],
+};
