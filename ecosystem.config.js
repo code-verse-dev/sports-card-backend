e2b75f9575bb584dc2dc63b07a848cf500ab56ea
ali-salsoft2025 <ali.hussain@salsoft.net>
2026-07-27 20:01:40 +0500

    latest

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "sports-card-backend",
-      script: "dist/index.js",
+      script: "index.js",
       instances: 1,
       autorestart: true,
       watch: false,
